82875373180a91d00ad9e5608903022199e47b92
chackhangun <chleorjs9@gmail.com>
2022-01-02 16:29:05 +0900

    add mbti test & fix min sdk version

--- app/src/main/java/com/camp/project1/MainActivity.java
+++ app/src/main/java/com/camp/project1/MainActivity.java
@@ -35,11 +35,20 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
     private com.camp.project1.PhonebookFragment phonebookFragment;
     private com.camp.project1.GalleryFragment galleryFragment;
-    public com.camp.project1.MbtiFragment mbtiFragment;
-    public com.camp.project1.MbtiFragment2 mbtiFragment2;
-    public com.camp.project1.MbtiFragment3 mbtiFragment3;
+    public com.camp.project1.MbtiQ1 mbtiQ1;
+    public com.camp.project1.MbtiQ2 mbtiQ2;
+    public com.camp.project1.MbtiQ3 mbtiQ3;
+    public com.camp.project1.MbtiQ4 mbtiQ4;
+    public com.camp.project1.MbtiQ5 mbtiQ5;
+    public com.camp.project1.MbtiQ6 mbtiQ6;
+    public com.camp.project1.MbtiQ7 mbtiQ7;
+    public com.camp.project1.MbtiQ8 mbtiQ8;
+    public com.camp.project1.MbtiQ9 mbtiQ9;
+    public com.camp.project1.MbtiQ10 mbtiQ10;
+    public com.camp.project1.MbtiQ11 mbtiQ11;
+    public com.camp.project1.MbtiQ12 mbtiQ12;
     public com.camp.project1.MbtiResult mbtiResult;
-
+    public MBTI mymbti;
     private static final int PERMISSION_NUM = 100;
     private static boolean permission_check = false;
     public static final String PROVIDER_URI = "content://com.camp.project1.ContactProvider";
@@ -54,16 +63,27 @@ public class MainActivity extends AppCompatActivity {
         bottomView.setOnNavigationItemSelectedListener(listener);
 
 
+        phonebookFragment = new PhonebookFragment();
+        galleryFragment = new GalleryFragment();
+        mbtiQ1 = new MbtiQ1();
+        mbtiQ2 = new MbtiQ2();
+        mbtiQ3 = new MbtiQ3();
+        mbtiQ4 = new MbtiQ4();
+        mbtiQ5 = new MbtiQ5();
+        mbtiQ6 = new MbtiQ6();
+        mbtiQ7 = new MbtiQ7();
+        mbtiQ8 = new MbtiQ8();
+        mbtiQ9 = new MbtiQ9();
+        mbtiQ10 = new MbtiQ10();
+        mbtiQ11 = new MbtiQ11();
+        mbtiQ12 = new MbtiQ12();
+        mbtiResult = new MbtiResult();
+        mymbti = new MBTI();
+
         if(permission_check == false){
             getpermission();
             permission_check = true;
         }
-        phonebookFragment = new PhonebookFragment();
-        galleryFragment = new GalleryFragment();
-        mbtiFragment = new MbtiFragment();
-        mbtiFragment2 = new MbtiFragment2();
-        mbtiFragment3 = new MbtiFragment3();
-        mbtiResult = new MbtiResult();
         /*
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED){
 
@@ -83,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, galleryFragment).commit();
                     break;
                 case R.id.item_calendar:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mbtiFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mbtiQ1).commit();
                     break;
             }
             return true;
---
+++ app/src/main/java/com/camp/project1/MbtiQ9.java
@@ -0,0 +1,57 @@
+package com.camp.project1;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
+
+public class MbtiQ9 extends Fragment implements View.OnClickListener{
+    public TextView answer1;
+    public TextView answer2;
+    MainActivity activity;
+    @Override
+    public void onAttach(Context context){ //fragment를 activity에 attach할때 호
+        super.onAttach(context);
+        activity = (MainActivity)getActivity();
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) { //초기화 리소스들
+        super.onCreate(savedInstanceState);
+    }
+    // view 객체 얻어서 초기화/ layout inflate하는곳출
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_mbti_q9, container, false);
+        answer1 = rootView.findViewById(R.id.Q9A1);
+        answer2 = rootView.findViewById(R.id.Q9A2);
+
+        answer1.setOnClickListener(this);
+        answer2.setOnClickListener(this);
+        return rootView;
+
+    }
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.Q9A1:
+                activity.mymbti.incrementPJ("J");
+                break;
+            case R.id.Q9A2:
+                activity.mymbti.incrementPJ("P");
+                break;
+        }
+        activity.replaceFragment(activity.mbtiQ10);
+    }
+}
