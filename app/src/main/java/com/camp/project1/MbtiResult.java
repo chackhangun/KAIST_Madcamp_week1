f0f9dff4f17a517866898680d0cb914fb2bd0c00
chackhangun <chleorjs9@gmail.com>
2022-01-02 20:41:32 +0900

    mbti kakao talk message  upgrade

--- app/src/main/java/com/camp/project1/MbtiResult.java
+++ app/src/main/java/com/camp/project1/MbtiResult.java
@@ -56,18 +56,18 @@ public class MbtiResult extends Fragment implements View.OnClickListener {
     }
 
     public void kakaolink() {
-        FeedTemplate params = FeedTemplate.newBuilder(ContentObject.newBuilder("디저트 사진",
-                        "http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+        FeedTemplate params = FeedTemplate.newBuilder(ContentObject.newBuilder("MBTI 테스",
+                        "https://i.imgur.com/LKAfF6P.png",
                         LinkObject.newBuilder().setWebUrl("https://developers.kakao.com")
                                 .setMobileWebUrl("https://developers.kakao.com").build())
-                        .setDescrption("아메리카노, 빵, 케익")
+                        .setDescrption("나의 MBTI는??")
                         .build())
                 .setSocial(SocialObject.newBuilder().setLikeCount(10).setCommentCount(20)
                         .setSharedCount(30).setViewCount(40).build())
-                .addButton(new ButtonObject("웹에서 보기", LinkObject.newBuilder().setWebUrl("'https://developers.kakao.com").setMobileWebUrl("'https://developers.kakao.com").build()))
-                .addButton(new ButtonObject("앱에서 보기", LinkObject.newBuilder()
-                        .setWebUrl("'https://developers.kakao.com")
-                        .setMobileWebUrl("'https://developers.kakao.com")
+                .addButton(new ButtonObject("와우 ", LinkObject.newBuilder().setWebUrl("https://i.imgur.com/LKAfF6P.png").setMobileWebUrl("https://i.imgur.com/LKAfF6P.png").build()))
+                .addButton(new ButtonObject("강준서 멍청", LinkObject.newBuilder()
+                        .setWebUrl("https://i.imgur.com/LKAfF6P.png")
+                        .setMobileWebUrl("https://i.imgur.com/LKAfF6P.png")
                         .setAndroidExecutionParams("key1=value1")
                         .setIosExecutionParams("key1=value1")
                         .build()))
